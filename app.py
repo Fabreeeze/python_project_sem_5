9e172005c760506bd74d76d5f08be3d234d8b6f4
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-10-06 19:32:50 +0530

    ninth commit, added some routes changes and new edit_data.html

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-from flask import Flask, render_template, request, jsonify
-from enhance_image import enhance_image
-from extract_information import extract_information
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+from extract_data import extract_information_from_ocr_results
+from save_to_mongo import save_to_mongodb
+import pytesseract
+from PIL import Image
 
 app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['hospital_db']  # Assuming 'hospital_db' is your database name
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/process_image', methods=['POST'])
-def process_image():
-    if 'file' not in request.files:
-        return jsonify({'message': 'No file part'})
-
+@app.route('/upload', methods=['POST'])
+def upload_file():
     file = request.files['file']
+    if file:
+        # Assuming you have a function process_image(file) which extracts data using OCR
+        process_image(file)
+        return "File uploaded and processed successfully!"
+    else:
+        return "No file uploaded"
 
-    if file.filename == '':
-        return jsonify({'message': 'No selected file'})
-
+def process_image(file):
     enhanced_image = enhance_image(file)
-    information = extract_information(enhanced_image)
+    ocr_text = pytesseract.image_to_string(enhanced_image)
+    extracted_data = extract_information_from_ocr_results(ocr_text)
+    save_to_mongodb(extracted_data)
 
-    # Store information in database
-    # Insert code for MongoDB or MySQL here
-
-    return jsonify({'message': 'Information extracted and stored successfully'})
+# def enhance_image(file):
+#     # Implement your image enhancement logic here
+#     # ...
 
 if __name__ == '__main__':
     app.run(debug=True)
