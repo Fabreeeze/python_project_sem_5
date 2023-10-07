38dd54d2860217126391d9065a7923accf9f4ceb
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-10-07 12:38:45 +0530

    commit eleven, removed incomplete mongo connection and added correct one, also added thumbnail of selected image to html page

--- save_to_mongo.py
+++ save_to_mongo.py
@@ -1,4 +1,18 @@
+from pymongo import MongoClient
+
 def save_to_mongodb(extracted_data):
-    
-    db.extracted_data.insert_one(extracted_data)
-    print("Data saved to MongoDB successfully!")
+    # Connect to MongoDB
+    client = MongoClient('mongodb://localhost:27017/') 
+
+    # Access the database
+    db = client['OCR_project']  
+
+    # Access the collection
+    collection = db['testing_data_collection'] 
+
+    # Insert the extracted data into the collection
+    collection.insert_one(extracted_data)
+
+    print("\nUwU ---\n\n\ndata stored\n\n\n\n----------\n\n\n")
+    # Close the connection when you're done
+    client.close()
--- static/script.js
+++ static/script.js
@@ -1,9 +1,21 @@
+document.getElementById('imageUpload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function() {
+        const thumbnailContainer = document.getElementById('thumbnailContainer');
+        thumbnailContainer.innerHTML = `<img src="${reader.result}" alt="Thumbnail" style="max-width: 200px;">`;
+    }
+
+    reader.readAsDataURL(file);
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
         const formData = new FormData(form);
-        const response = await fetch('/process_image', {
+        const response = await fetch('/upload', {
             method: 'POST',
             body: formData,
         });
