9e7902635cfbe8c6d8c516f84cc3fcb077ce6bf6
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-10-08 02:27:45 +0530

    commit twelve , added navbar

--- static/script.js
+++ static/script.js
@@ -1,14 +1,37 @@
+function showUploadTab() {
+    document.getElementById('uploadTab').style.display = 'block';
+    document.getElementById('databaseTab').style.display = 'none';
+}
+
+function showDatabaseTab() {
+    document.getElementById('uploadTab').style.display = 'none';
+    document.getElementById('databaseTab').style.display = 'block';
+}
+
+
+
 document.getElementById('imageUpload').addEventListener('change', function(event) {
+    const previewImage = document.getElementById('previewImage');
     const file = event.target.files[0];
     const reader = new FileReader();
 
     reader.onload = function() {
-        const thumbnailContainer = document.getElementById('thumbnailContainer');
-        thumbnailContainer.innerHTML = `<img src="${reader.result}" alt="Thumbnail" style="max-width: 200px;">`;
-    }
+        previewImage.src = reader.result;
+    };
 
     reader.readAsDataURL(file);
+
+    const removeIcon = document.getElementById("remove_icon");
+    removeIcon.style.display = 'block';
 });
+
+function removeImage() {
+    const previewImage = document.getElementById('previewImage');
+    previewImage.src = "#";
+
+    const removeIcon = document.getElementById("remove_icon");
+    removeIcon.style.display = 'none';
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
