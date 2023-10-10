56fb9662a9ebcc5ac2c8b0a03aa8fb6ecbda99a5
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-10-10 21:56:51 +0530

    commit fifteen, changed insecure open db connection

--- save_to_mongo.py
+++ save_to_mongo.py
@@ -1,15 +1,17 @@
 from pymongo import MongoClient
 
- # Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/') 
 
-# Access the database
-db = client['OCR_project']  
-
-# Access the collection
-collection = db['testing_data_collection'] 
 
 def save_to_mongodb(extracted_data):
+
+     # Connect to MongoDB
+    client = MongoClient('mongodb://localhost:27017/') 
+
+    # Access the database
+    db = client['OCR_project']  
+
+    # Access the collection
+    collection = db['testing_data_collection'] 
 
     # Insert the extracted data into the collection
     collection.insert_one(extracted_data)
@@ -17,8 +19,19 @@ def save_to_mongodb(extracted_data):
     print("\nUwU ---\n\n\ndata stored\n\n\n\n----------\n\n\n")
     # Close the connection when you're done
     
+    db.close()
 
 
 def load_from_mongodb():
+        # Connect to MongoDB
+    client = MongoClient('mongodb://localhost:27017/') 
+
+    # Access the database
+    db = client['OCR_project']  
+
+    # Access the collection
+    collection = db['testing_data_collection'] 
     data = db.testing_data_collection.find()  # Fetch data from MongoDB
+    
+    db.close()
     return data
