1c87b4e19de37bb44ebfc3a79bc8729bff8fdc1e
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-10-20 16:33:40 +0530

    commit 29, corrected editdata.html connection

--- static/edit_data_script.js
+++ static/edit_data_script.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const form = document.querySelector('form');
+// document.addEventListener('DOMContentLoaded',() => {
+//     const form = document.querySelector('form');
 
 
 
-    form.addEventListener('submit', async(e) =>{
-        e.preventDefault();
-        const formData = new FormData(form);
-        const response = await fetch('/submit_edited_data',{
-            method : 'POST',
-            body : JSON.stringify(formData.get('edited_text'))
-        })
-        const result = await response.json();
-        console.log("formdata = "+formData);
-        console.log("result = "+result);
-        if(result.success){
-            console.log("image data successfully stored in db");
-        } else {
-            console.error('Error saving data to the database');
-        }
-    })
-})
+//     form.addEventListener('submit', async(e) =>{
+//         e.preventDefault();
+//         const formData = new FormData(form);
+//         const response = await fetch('/submit_edited_data',{
+//             method : 'POST',
+//             body : JSON.stringify(formData.get('edited_text'))
+//         })
+//         const result = await response.json();
+//         console.log("formdata = "+formData);
+//         console.log("result = "+result);
+//         if(result.success){
+//             console.log("image data successfully stored in db");
+//         } else {
+//             console.error('Error saving data to the database');
+//         }
+//     })
+// })
--- static/success_message_script.js
+++ static/success_message_script.js
@@ -1,36 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const formData = new FormData(form);
-        const response = await fetch('/upload', {
-            method: 'POST',
-            body: formData,
-        });
-        const result = await response.json();
-        console.log("data sent"); 
-        console.log(result);
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.querySelector('form');
+//     form.addEventListener('submit', async (e) => {
+//         e.preventDefault();
+//         const formData = new FormData(form);
+//         console.log(formData);
+//         const response = await fetch('/upload', {
+//             method: 'POST',
+//             body: formData,
+//             // headers: {
+//             //     'Content-type': 'multipart/form-data'
+//             // }
+//         });
+//         console.log("response=",JSON.stringify(response));
+//         // if (response.ok) {
+//         //     const result = await response.json();
+//         //     console.log("ok result=", result);
+//         // } else {
+//         //     console.error('nope nope nope !!! Error:', response.status);
+//         // }
 
-        if (result.success) {
-            const successMessage = document.createElement('div');
-            successMessage.classList.add('alert', 'alert-success', 'mt-4','fixed-top', 'text-center', 'w-100');
-            successMessage.innerText = 'Image successfully scanned and uploaded!';
-            document.body.appendChild(successMessage);
 
-            // Set a timeout to remove the success message after 3 seconds
-            setTimeout(function() {
-                successMessage.remove();
-            }, 5000);
+//     //     const result = await response.json();
+//     //     console.log("data sent"); 
+//     //     console.log("result= ", result);
 
-            const imagePreview = document.querySelector('.image-preview');
-            if (imagePreview) {
-                imagePreview.remove();
-            }
+//     //     if (result.success) {
+//     //         const successMessage = document.createElement('div');
+//     //         successMessage.classList.add('alert', 'alert-success', 'mt-4','fixed-top', 'text-center', 'w-100');
+//     //         successMessage.innerText = 'Image successfully scanned and uploaded!';
+//     //         document.body.appendChild(successMessage);
 
-        } 
-        else {
-            console.log("here ===== "+response);
-            console.error('Error uploading image');
-        }
-    });
-});
+//     //         // Set a timeout to remove the success message after 3 seconds
+//     //         setTimeout(function() {
+//     //             successMessage.remove();
+//     //         }, 5000);
+
+//     //         const imagePreview = document.querySelector('.image-preview');
+//     //         if (imagePreview) {
+//     //             imagePreview.remove();
+//     //         }
+
+//     //     } 
+//     //     else {
+//     //         console.log("here ===== "+response);
+//     //         console.error('Error uploading image');
+//     //     }
+//     });
+// });
